7f736c8a98e2f522c893766ac1e2400caa852784
kalymos <schumachernicolas@gmail.com>
2018-06-11 05:14:07 +0200

    Update and rename Keypad.cpp to Numpad.cpp

--- src/Numpad.cpp
+++ src/Numpad.cpp
@@ -29,68 +29,68 @@
 || #
 ||
 */
-#include <Keypad.h>
+#include <Numpad.h>
 
 // <<constructor>> Allows custom keymap, pin configuration, and keypad sizes.
-Keypad::Keypad(char *userKeymap, byte *row, byte *col, byte numRows, byte numCols) {
+Numpad::Numpad(char *userNummap, byte *row, byte *col, byte numRows, byte numCols) {
 	rowPins = row;
 	columnPins = col;
-	sizeKpd.rows = numRows;
-	sizeKpd.columns = numCols;
+	sizeNpd.rows = numRows;
+	sizeNpd.columns = numCols;
 
-	begin(userKeymap);
+	begin(userNummap);
 
 	setDebounceTime(10);
 	setHoldTime(500);
-	keypadEventListener = 0;
+	NumpadEventListener = 0;
 
 	startTime = 0;
 	single_key = false;
 }
 
 // Let the user define a keymap - assume the same row/column count as defined in constructor
-void Keypad::begin(char *userKeymap) {
-    keymap = userKeymap;
+void Numpad::begin(char *userNummap) {
+    Nummap = userNummap;
 }
 
 // Returns a single key only. Retained for backwards compatibility.
-char Keypad::getKey() {
-	single_key = true;
+int Numpad::getNum() {
+	single_Num = true;
 
-	if (getKeys() && key[0].stateChanged && (key[0].kstate==PRESSED))
-		return key[0].kchar;
+	if (getNum() && num[0].stateChanged && (num[0].kstate==PRESSED))
+		return num[0].nint;
 	
-	single_key = false;
+	single_num = false;
 
-	return NO_KEY;
+	return NO_NUM;
 }
 
 // Populate the key list.
-bool Keypad::getKeys() {
-	bool keyActivity = false;
+bool Numpad::getNum() {
+	bool numActivity = false;
 
 	// Limit how often the keypad is scanned. This makes the loop() run 10 times as fast.
 	if ( (millis()-startTime)>debounceTime ) {
-		scanKeys();
-		keyActivity = updateList();
+		scanNum();
+		numActivity = updateList();
 		startTime = millis();
 	}
 
-	return keyActivity;
+	return numActivity;
 }
 
 // Private : Hardware scan
-void Keypad::scanKeys() {
+void Numpad::scanNum() {
 	// Re-intialize the row pins. Allows sharing these pins with other hardware.
 	for (byte r=0; r<sizeKpd.rows; r++) {
 		pin_mode(rowPins[r],INPUT_PULLUP);
 	}
 
 	// bitMap stores ALL the keys that are being pressed.
-	for (byte c=0; c<sizeKpd.columns; c++) {
+	for (byte c=0; c<sizeNpd.columns; c++) {
 		pin_mode(columnPins[c],OUTPUT);
 		pin_write(columnPins[c], LOW);	// Begin column pulse output.
-		for (byte r=0; r<sizeKpd.rows; r++) {
+		for (byte r=0; r<sizeNpd.rows; r++) {
 			bitWrite(bitMap[r], c, !pin_read(rowPins[r]));  // keypress is active low so invert to high.
 		}
 		// Set pin to high impedance input. Effectively ends column pulse.
@@ -100,38 +100,38 @@ void Keypad::scanKeys() {
 }
 
 // Manage the list without rearranging the keys. Returns true if any keys on the list changed state.
-bool Keypad::updateList() {
+bool Numpad::updateList() {
 
 	bool anyActivity = false;
 
 	// Delete any IDLE keys
 	for (byte i=0; i<LIST_MAX; i++) {
-		if (key[i].kstate==IDLE) {
-			key[i].kchar = NO_KEY;
-			key[i].kcode = -1;
-			key[i].stateChanged = false;
+		if (num[i].kstate==IDLE) {
+			num[i].nint = NO_KEY;
+			num[i].ncode = -1;
+			num[i].stateChanged = false;
 		}
 	}
 
 	// Add new keys to empty slots in the key list.
 	for (byte r=0; r<sizeKpd.rows; r++) {
-		for (byte c=0; c<sizeKpd.columns; c++) {
+		for (byte c=0; c<sizeNpd.columns; c++) {
 			boolean button = bitRead(bitMap[r],c);
-			char keyChar = keymap[r * sizeKpd.columns + c];
-			int keyCode = r * sizeKpd.columns + c;
-			int idx = findInList (keyCode);
+			char numInt = nummap[r * sizenpd.columns + c];
+			int numCode = r * sizeKpd.columns + c;
+			int idx = findInList (numCode);
 			// Key is already on the list so set its next state.
 			if (idx > -1)	{
-				nextKeyState(idx, button);
+				nextnumState(idx, button);
 			}
 			// Key is NOT on the list so add it.
 			if ((idx == -1) && button) {
 				for (byte i=0; i<LIST_MAX; i++) {
-					if (key[i].kchar==NO_KEY) {		// Find an empty slot or don't add key to list.
-						key[i].kchar = keyChar;
-						key[i].kcode = keyCode;
-						key[i].kstate = IDLE;		// Keys NOT on the list have an initial state of IDLE.
-						nextKeyState (i, button);
+					if (num[i].kchar==NO_KEY) {		// Find an empty slot or don't add key to list.
+						num[i].nint = keyChar;
+						num[i].ncode = keyCode;
+						num[i].nstate = IDLE;		// Keys NOT on the list have an initial state of IDLE.
+						nextNumState (i, button);
 						break;	// Don't fill all the empty slots with the same key.
 					}
 				}
@@ -141,7 +141,7 @@ bool Keypad::updateList() {
 
 	// Report if the user changed the state of any key.
 	for (byte i=0; i<LIST_MAX; i++) {
-		if (key[i].stateChanged) anyActivity = true;
+		if (num[i].stateChanged) anyActivity = true;
 	}
 
 	return anyActivity;
@@ -149,10 +149,10 @@ bool Keypad::updateList() {
 
 // Private
 // This function is a state machine but is also used for debouncing the keys.
-void Keypad::nextKeyState(byte idx, boolean button) {
-	key[idx].stateChanged = false;
+void Numpad::nextNumState(byte idx, boolean button) {
+	num[idx].stateChanged = false;
 
-	switch (key[idx].kstate) {
+	switch (num[idx].nstate) {
 		case IDLE:
 			if (button==CLOSED) {
 				transitionTo (idx, PRESSED);
@@ -175,10 +175,10 @@ void Keypad::nextKeyState(byte idx, boolean button) {
 }
 
 // New in 2.1
-bool Keypad::isPressed(char keyChar) {
+bool Numpad::isPressed(int numInt) {
 	for (byte i=0; i<LIST_MAX; i++) {
-		if ( key[i].kchar == keyChar ) {
-			if ( (key[i].kstate == PRESSED) && key[i].stateChanged )
+		if ( num[i].nint == numInt ) {
+			if ( (num[i].kntate == PRESSED) && num[i].stateChanged )
 				return true;
 		}
 	}
@@ -187,9 +187,9 @@ bool Keypad::isPressed(char keyChar) {
 
 // Search by character for a key in the list of active keys.
 // Returns -1 if not found or the index into the list of active keys.
-int Keypad::findInList (char keyChar) {
+int Numpad::findInList (int numInt) {
 	for (byte i=0; i<LIST_MAX; i++) {
-		if (key[i].kchar == keyChar) {
+		if (num[i].nchar == numInt) {
 			return i;
 		}
 	}
@@ -198,9 +198,9 @@ int Keypad::findInList (char keyChar) {
 
 // Search by code for a key in the list of active keys.
 // Returns -1 if not found or the index into the list of active keys.
-int Keypad::findInList (int keyCode) {
+int Numpad::findInList (int numCode) {
 	for (byte i=0; i<LIST_MAX; i++) {
-		if (key[i].kcode == keyCode) {
+		if (num[i].kcode == numCode) {
 			return i;
 		}
 	}
@@ -208,58 +208,58 @@ int Keypad::findInList (int keyCode) {
 }
 
 // New in 2.0
-char Keypad::waitForKey() {
-	char waitKey = NO_KEY;
-	while( (waitKey = getKey()) == NO_KEY );	// Block everything while waiting for a keypress.
+int Numpad::waitForNum() {
+	int waitNum = NO_NUM;
+	while( (waitNum = getNum()) == NO_NUM );	// Block everything while waiting for a keypress.
 	return waitKey;
 }
 
 // Backwards compatibility function.
-KeyState Keypad::getState() {
-	return key[0].kstate;
+NumState Numpad::getState() {
+	return num[0].nstate;
 }
 
 // The end user can test for any changes in state before deciding
 // if any variables, etc. needs to be updated in their code.
-bool Keypad::keyStateChanged() {
-	return key[0].stateChanged;
+bool Numpad::numStateChanged() {
+	return num[0].stateChanged;
 }
 
 // The number of keys on the key list, key[LIST_MAX], equals the number
 // of bytes in the key list divided by the number of bytes in a Key object.
-byte Keypad::numKeys() {
-	return sizeof(key)/sizeof(Key);
+byte Numpad::numNum() {
+	return sizeof(num)/sizeof(num);
 }
 
 // Minimum debounceTime is 1 mS. Any lower *will* slow down the loop().
-void Keypad::setDebounceTime(uint debounce) {
+void Numpad::setDebounceTime(uint debounce) {
 	debounce<1 ? debounceTime=1 : debounceTime=debounce;
 }
 
-void Keypad::setHoldTime(uint hold) {
+void Numpad::setHoldTime(uint hold) {
     holdTime = hold;
 }
 
-void Keypad::addEventListener(void (*listener)(char)){
-	keypadEventListener = listener;
+void Numpad::addEventListener(void (*listener)(int)){
+	numpadEventListener = listener;
 }
 
-void Keypad::transitionTo(byte idx, KeyState nextState) {
-	key[idx].kstate = nextState;
-	key[idx].stateChanged = true;
+void Numpad::transitionTo(byte idx, NumState nextState) {
+	num[idx].nstate = nextState;
+	num[idx].stateChanged = true;
 
 	// Sketch used the getKey() function.
 	// Calls keypadEventListener only when the first key in slot 0 changes state.
-	if (single_key)  {
-	  	if ( (keypadEventListener!=NULL) && (idx==0) )  {
-			keypadEventListener(key[0].kchar);
+	if (single_num)  {
+	  	if ( (numpadEventListener!=NULL) && (idx==0) )  {
+			numpadEventListener(num[0].nint);
 		}
 	}
 	// Sketch used the getKeys() function.
 	// Calls keypadEventListener on any key that changes state.
 	else {
-	  	if (keypadEventListener!=NULL)  {
-			keypadEventListener(key[idx].kchar);
+	  	if (numpadEventListener!=NULL)  {
+			numpadEventListener(num[idx].nchar);
 		}
 	}
 }
