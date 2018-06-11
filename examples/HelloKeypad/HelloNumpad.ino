c5a20700ebf0cdde9acc8465c9e7f41007f63d66
kalymos <schumachernicolas@gmail.com>
2018-06-11 05:24:33 +0200

    Update and rename HelloKeypad.ino to HelloNumpad.ino

--- examples/HelloKeypad/HelloNumpad.ino
+++ examples/HelloKeypad/HelloNumpad.ino
@@ -7,29 +7,29 @@
 || | Demonstrates the simplest use of the matrix Keypad library.
 || #
 */
-#include <Keypad.h>
+#include <Numpad.h>
 
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
-char keys[ROWS][COLS] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'}
+int Nums[ROWS][COLS] = {
+  {1,2,3},
+  {4,5,6},
+  {7,8,9},
+  {10,11,12}
 };
 byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+Numpad numpad = Numpad( makeNummap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup(){
   Serial.begin(9600);
 }
   
 void loop(){
-  char key = keypad.getKey();
+  int num = numpad.getNum();
   
-  if (key){
-    Serial.println(key);
+  if (num){
+    Serial.println(num);
   }
 }
